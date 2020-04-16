6b8a8debf4f807957bfe08f1afc18704a812de17
zagorski32611 <jzagorski@banyantechnology.com>
2020-04-16 08:38:14 -0400

    created base model

--- Models/WeatherReports.cs
+++ Models/WeatherReports.cs
@@ -7,6 +7,13 @@ namespace weatherMvc.Models
 {
     public class WeatherReport : WeatherData
     {
+        // first define fields unique to the reports, then bring in the weather data:
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        public WeatherData weather { get; set; }
+
+        public LocationData locationData { get; set; }
 
     }
 }
