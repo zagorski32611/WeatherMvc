dab116bbd24ce082a6e8d6288fc2eea6f262ab03
zagorski32611 <jzagorski@banyantechnology.com>
2020-05-15 17:20:30 -0400

    Add some stuff

---
+++ Interfaces/IWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using weatherMvc.Models;
+
+namespace weatherMvc.Interfaces
+{
+    interface IWeatherService
+    {
+        public Task<WeatherData> CallDarkSky(double lat, double lng);
+    }
+}
---
+++ Services/WeatherService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using weatherMvc.Models;
+using Newtonsoft.Json;
+using weatherMvc.Data;
+
+namespace weatherMvc.Services
+{
+    public class WeatherService
+    {
+        private readonly WeatherMvcDbContext _context;
+
+        public async Task<WeatherData> CallDarkSky(double lat, double lng)
+        {
+            WeatherData weather_data = new WeatherData();
+            HttpClient httpclient = new HttpClient();
+
+            string weather_uri = $"https://api.darksky.net/forecast/dcd2262dfdbb2349f6e41e54e7a8d40a/{lat},{lng}?exclude=minutely,hourly";
+            // 41.443423,-81.775168
+
+            try
+            {
+                HttpResponseMessage response = await httpclient.GetAsync($"{weather_uri}");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                WeatherData deserializedWeather = JsonConvert.DeserializeObject<WeatherData>(responseBody);
+
+                weather_data = deserializedWeather;
+
+                return weather_data;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot Reach Dark Sky...");
+                Console.WriteLine($"Reason: {e}");
+                return weather_data;
+            }
+            finally
+            {
+                SaveWeatherData(weather_data);
+            }
+        }
+
+        public async void SaveWeatherData(WeatherData weatherData)
+        {
+            try
+            {
+                _context.Add(weatherData);
+                await _context.SaveChangesAsync();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("${e}");
+            }
+
+        }
+    }
+}
